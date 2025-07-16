e33aa39496b4e71f9d59e3d6e50214fdd4c63efc
Kittiphat S <kittiphat@muze.co.th>
2025-07-17 00:55:57 +0700

    chore: prettier

--- packages/core/src/utils/generator.ts
+++ packages/core/src/utils/generator.ts
@@ -69,4 +69,3 @@ export function generateOTPAndReference(
     reference: generateReference(referenceConfig)
   }
 }
-
--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -5,4 +5,3 @@ export {
   generateReference,
   generateOTPAndReference
 } from './generator'
-
